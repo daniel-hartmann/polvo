91d45ebefaecf2605f946a6338625bc1f8a347c8
Daniel Hartmann <labs_danielh@keystonestrategy.com>
2024-04-07 23:54:36 -0300

    Fixed python3.9 install

--- setup.sh
+++ setup.sh
@@ -22,6 +22,12 @@ then
     echo 'eval "$(pyenv init -)"' >> ~/.bashrc
     echo 'eval "$(pyenv init -)"' >> ~/.profile
     . ~/.bashrc
+    . ~/.profile
+
+    sudo apt install build-essential libssl-dev zlib1g-dev \
+        libbz2-dev libreadline-dev libsqlite3-dev curl \
+        libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
+
     pyenv install 3.9
     pyenv global 3.9
 fi
